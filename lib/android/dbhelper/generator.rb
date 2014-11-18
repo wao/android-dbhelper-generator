0f0c27de7d6a11a11b4f5e7127e31ca18a95550d
Yang Chen <mike.c.yang@gmail.com>
2014-11-18 10:17:21 +0800

    Port Sequel::Schema::Generator for define schema

--- lib/android/dbhelper/generator.rb
+++ lib/android/dbhelper/generator.rb
@@ -1,6 +1,32 @@
+require 'android/dbhelper/schema_generator'
+
 module Android; end
 module Android::Dbhelper; end
 
 class Android::Dbhelper::Generator
   VERSION = "1.0.0"
+
+  attr_reader :tables
+
+  Table = Struct.new( :name, :schema, :options )
+  class Sqlite
+        def serial_primary_key_options
+            {}
+        end
+  end
+
+
+  def initialize(block)
+      @tables={}
+      @db = Sqlite.new
+      instance_exec(nil, &block)
+  end
+
+  def self.declare(&block)
+      Android::Dbhelper::Generator.new(block)
+  end
+
+  def create_table(name,options={},&block)
+      @tables[name]= Table.new( Android::Dbhelper::Schema::Generator.new(@db, &block), options )
+  end
 end
--- test/android/dbhelper/test_generator.rb
+++ test/android/dbhelper/test_generator.rb
@@ -1,12 +1,23 @@
-gem "minitest"
-require "minitest/autorun"
-require "android/dbhelper/generator"
+#!/usr/bin/env ruby
+require 'test/unit'
+require 'byebug'
+require 'pp'
 
-module TestAndroid; end
-module TestAndroid::TestDbhelper; end
+require File.dirname(__FILE__) + "/../../test_helper.rb"
 
-class TestAndroid::TestDbhelper::TestGenerator < Minitest::Test
-  def test_sanity
-    flunk "write tests or I will kneecap you"
-  end
+require 'android/dbhelper/generator'
+
+class TestAndroidDbHelperGenerator < Test::Unit::TestCase
+    def test_create_table
+        generator = Android::Dbhelper::Generator.declare do
+            create_table( :TestTable ) do
+                primary_key :id
+                String :abcdef, :null=>false
+            end
+        end
+
+        pp generator
+
+        assert !generator.tables[:TestTable].nil?
+    end
 end
